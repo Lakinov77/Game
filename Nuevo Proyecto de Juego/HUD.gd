7fc8f1a2f01ea0ed174a46c33d42d85ee79ff859
Lakinov77 <89871248+Lakinov77@users.noreply.github.com>
2022-08-15 22:08:49 -0600

    Add files via upload

---
+++ Nuevo Proyecto de Juego/HUD.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+signal start_game
+func _ready():
+	pass 
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
+func show_game_over():
+	show_message("Game Over")
+	
+	yield($MessageTimer, "timeout")
+	$Message.text = "Sammy"
+	$Message.show()
+	
+	yield(get_tree().create_timer(1), "timeout")
+	$StartButtom.show()
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
+func _on_MessageTimer_timeout():
+	$MessageTimer.hide()
